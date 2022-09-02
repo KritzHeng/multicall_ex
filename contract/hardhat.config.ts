f279318191c17a7f8adf2b3bf33f8b3f4f2f13ce
KritzHeng <mr.kriskanin@gmail.com>
2022-09-02 14:57:43 +0700

    add typechain

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,8 +1,73 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const getAccounts = () => {
+  const arr = Object.entries(process.env);
+  const privateKeys = arr
+    .filter(([key, val]) => key.includes(`PRIVATE_KEY`))
+    .map(([key, val]) => val || "");
+  return privateKeys;
+};
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.8.9",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161/`,
+      accounts: getAccounts(),
+    },
+  },
 };
 
 export default config;
